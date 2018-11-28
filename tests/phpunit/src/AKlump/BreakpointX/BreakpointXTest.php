f29c96131e55feca9c05da36170a560ad3cdbf0b
Aaron Klump <sourcecode@intheloftstudios.com>
2018-11-27 16:55:26 -0800

    Reformat php.

--- tests/phpunit/src/AKlump/BreakpointX/BreakpointXTest.php
+++ tests/phpunit/src/AKlump/BreakpointX/BreakpointXTest.php
@@ -8,92 +8,83 @@ namespace AKlump\BreakpointX;
  */
 class BreakpointXTest extends \PHPUnit_Framework_TestCase {
 
-    public function testQuery()
-    {
-        $setting = array(
-            "small"  => 0,
-            "mobile" => 480,
-            "desktop"  => 768,
-        );
-        $obj = new BreakpointX($setting);
-        $this->assertSame('max-width: 479px', $obj->query('small'));
-        $this->assertSame('max-width: 767px', $obj->query('mobile'));
-        $this->assertSame('min-width: 768px', $obj->query('desktop'));
-    }
-    public function testGop()
-    {
-        $setting = array(
-            "mobile"  => 0,
-            "desktop"  => 768,
-        );
-        $obj = new BreakpointX($setting);
-        $this->assertSame($setting, $obj->getSetting('breakpoints'));
-        $this->assertSame(array_keys($setting), $obj->aliases);
-    }
+  public function testQuery() {
+    $setting = array(
+      "small" => 0,
+      "mobile" => 480,
+      "desktop" => 768,
+    );
+    $obj = new BreakpointX($setting);
+    $this->assertSame('max-width: 479px', $obj->query('small'));
+    $this->assertSame('max-width: 767px', $obj->query('mobile'));
+    $this->assertSame('min-width: 768px', $obj->query('desktop'));
+  }
 
-    public function testGetSettings()
-    {
-        $control = [0, 480, 768];
-        $this->assertSame($control, $this->obj->getSetting('breakpoints'));
-        $this->assertNull($this->obj->getSetting('lunch'));
-        $this->assertSame(1, $this->obj->getSetting('index', 1));
-    }
+  public function testGop() {
+    $setting = array(
+      "mobile" => 0,
+      "desktop" => 768,
+    );
+    $obj = new BreakpointX($setting);
+    $this->assertSame($setting, $obj->getSetting('breakpoints'));
+    $this->assertSame(array_keys($setting), $obj->aliases);
+  }
 
-    public function testFirst()
-    {
-        $this->assertSame("max-width: 479px", $this->obj->alias('first'));
-    }
+  public function testGetSettings() {
+    $control = [0, 480, 768];
+    $this->assertSame($control, $this->obj->getSetting('breakpoints'));
+    $this->assertNull($this->obj->getSetting('lunch'));
+    $this->assertSame(1, $this->obj->getSetting('index', 1));
+  }
 
-    public function testLast()
-    {
-        $this->assertSame("min-width: 768px", $this->obj->alias('last'));
-    }
+  public function testFirst() {
+    $this->assertSame("max-width: 479px", $this->obj->alias('first'));
+  }
 
-    /**
-     * Provides data for testValue.
-     */
-    function DataForTestValueProvider()
-    {
-        $tests = array();
-        $tests[] = array([0, 479], "max-width: 479px", [100, 320]);
-        $tests[] = array([480, 767], "max-width: 767px", [500, 600]);
-        $tests[] = array([768, null], "min-width: 768px", [800, 2560]);
+  public function testLast() {
+    $this->assertSame("min-width: 768px", $this->obj->alias('last'));
+  }
 
-        return $tests;
-    }
+  /**
+   * Provides data for testValue.
+   */
+  function DataForTestValueProvider() {
+    $tests = array();
+    $tests[] = array([0, 479], "max-width: 479px", [100, 320]);
+    $tests[] = array([480, 767], "max-width: 767px", [500, 600]);
+    $tests[] = array([768, NULL], "min-width: 768px", [800, 2560]);
 
-    /**
-     * @dataProvider DataForTestValueProvider
-     */
-    public function testAlias($extremes, $alias, $other)
-    {
-        $subjects = array_filter(array_merge($extremes, $other));
-        foreach ($subjects as $x) {
-            $this->assertSame($alias, $this->obj->alias($x));
-        }
-    }
+    return $tests;
+  }
 
-    /**
-     * @dataProvider DataForTestValueProvider
-     */
-    public function testValue($control, $alias)
-    {
-        $this->assertSame($control, $this->obj->value($alias));
+  /**
+   * @dataProvider DataForTestValueProvider
+   */
+  public function testAlias($extremes, $alias, $other) {
+    $subjects = array_filter(array_merge($extremes, $other));
+    foreach ($subjects as $x) {
+      $this->assertSame($alias, $this->obj->alias($x));
     }
+  }
 
-    public function testConstruct()
-    {
-        $control = [
-            "max-width: 479px" => [0, 479],
-            "max-width: 767px" => [480, 767],
-            "min-width: 768px" => [768, null],
-        ];
-        $this->assertSame($control, $this->obj->breakpoints);
-        $this->assertSame(array_keys($control), $this->obj->aliases);
-    }
+  /**
+   * @dataProvider DataForTestValueProvider
+   */
+  public function testValue($control, $alias) {
+    $this->assertSame($control, $this->obj->value($alias));
+  }
 
-    public function setUp()
-    {
-        $this->obj = new BreakpointX([0, 480, 768]);
-    }
+  public function testConstruct() {
+    $control = [
+      "max-width: 479px" => [0, 479],
+      "max-width: 767px" => [480, 767],
+      "min-width: 768px" => [768, NULL],
+    ];
+    $this->assertSame($control, $this->obj->breakpoints);
+    $this->assertSame(array_keys($control), $this->obj->aliases);
+  }
+
+  public function setUp() {
+    $this->obj = new BreakpointX([0, 480, 768]);
+  }
 }
