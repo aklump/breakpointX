f76bba6064e25fcfde720170a4a1b068f58f8187
Aaron Klump <sourcecode@intheloftstudios.com>
2018-11-28 13:21:18 -0800

    Fix broken js tests.

--- .web_package/hooks/build/50_dist.sh
+++ .web_package/hooks/build/50_dist.sh
@@ -14,7 +14,6 @@ test -e "$7/README.md" && cp "$7/README.md" "$7/dist/"
 test -e "$7/CHANGELOG.md" && cp "$7/CHANGELOG.md" "$7/dist/"
 cp "$7/composer.json" "$7/dist/"
 cp "$7/BreakpointX.js" "$7/dist/"
-cp "$7/BreakpointX.min.js" "$7/dist/"
 cp "$7/BreakpointX.php" "$7/dist/"
 cp "$7/version.yml" "$7/dist/"
 
