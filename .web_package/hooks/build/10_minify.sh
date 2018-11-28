441b44c693476f8ecf3b3745538368b8fa07a4ef
Aaron Klump <sourcecode@intheloftstudios.com>
2018-11-28 15:22:55 -0800

    Remove /dist since we will be publishing to npm.

--- .web_package/hooks/build/10_minify.sh
+++ .web_package/hooks/build/10_minify.sh
@@ -2,7 +2,8 @@
 
 source "${11}/functions.sh"
 
-$7/node_modules/.bin/uglifyjs --compress --mangle --output=dist/BreakpointX.min.js --comments -- BreakpointX.js
+$7/node_modules/.bin/uglifyjs --compress --mangle --output=BreakpointX.min.js --comments -- BreakpointX.js
 
 # Verify the minified assets were built.
-wp_wait_for_exists "$7/dist/BreakpointX.min.js"
+wp_wait_for_exists "$7/BreakpointX.min.js"
+cd "$7" && git add BreakpointX.min.js
