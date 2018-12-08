1eedd8f597fc09f1b62810d2c8b044fb41a29b03
Aaron Klump <sourcecode@intheloftstudios.com>
2018-12-08 08:29:22 -0800

    Add not in build about manual qunit.

--- .web_package/hooks/build/50_run_tests.php
+++ .web_package/hooks/build/50_run_tests.php
@@ -8,5 +8,6 @@
 namespace AKlump\WebPackage;
 
 $build
+  ->caution("You should manually run tests/qunit/test.html; as they are not yet automated.")
   ->runTests('phpunit.xml')
   ->displayMessages();
