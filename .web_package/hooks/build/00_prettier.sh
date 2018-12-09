b9196bd558c03d7ab59dbaa7db8f9fb81a77018a
Aaron Klump <sourcecode@intheloftstudios.com>
2018-12-09 13:17:57 -0800

    Update config.

--- .web_package/hooks/build/00_prettier.sh
+++ .web_package/hooks/build/00_prettier.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Have to change into the directory of the config file is not found.
-cd src && ../node_modules/.bin/prettier "*.js" --write
+(cd src && ../node_modules/.bin/prettier "*.js" --write)
+(cd tests/qunit && ../../node_modules/.bin/prettier "*.js" --write)
